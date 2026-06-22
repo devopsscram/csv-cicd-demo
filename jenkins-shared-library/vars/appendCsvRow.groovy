77a67ed70c7ad7b5ff8299af9b95d3a3bfeb5ad7
devopsscram <himalayanscram9@gmail.com>
2026-06-22 17:56:36 +0530

    Update appendCsvRow.groovy

--- jenkins-shared-library/vars/appendCsvRow.groovy
+++ jenkins-shared-library/vars/appendCsvRow.groovy
@@ -1,1 +1,11 @@
+def call(Map config) {
 
+    def csvFile = config.csvFile
+    def rowData = config.rowData
+
+    sh """
+        echo '${rowData}' >> ${csvFile}
+    """
+
+    echo "Row appended successfully"
+}
